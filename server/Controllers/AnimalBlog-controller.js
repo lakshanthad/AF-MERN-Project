b33256178583d12bf04f7d57879c14e8c02d28a5
Sandali Disara <sandalidissanayake123@gmail.com>
2023-05-02 19:57:14 +0530

    create animalBlog create and view all controller classes

---
+++ server/Controllers/AnimalBlog-controller.js
@@ -0,0 +1,32 @@
+const AnimalBlog = require("../Models/AnimalBlog");
+
+//add a animal Blog
+const addAnimalBlog = async (req, res, next) => {
+  const { Title, Description, Thumbnail } = req.body;
+
+  const newAnimalBlog = new AnimalBlog({
+    Title,
+    Description,
+    Thumbnail,
+  });
+
+  newAnimalBlog
+    .save()
+    .then(() => {
+      res.json("Animal Blog Added");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+//read all animal Blogs
+const getAllAnimalBlogs = async (req, res, next) => {
+  AnimalBlog.find()
+    .then((animalblogs) => {
+      res.json(animalblogs);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
