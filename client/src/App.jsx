012de0720f64c1700319b96d00e208515395ca08
mehara00 <119718801+MeharaSahabandu@users.noreply.github.com>
2023-05-03 23:24:44 +0530

    landing pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
-// import React from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Animal from './Components/Animal';
+import Animal from "./Components/Animal";
+import LandingPageGrassroot from "./Components/LandingPageGrassroot";
+import LandingPageStaff from "./Components/LandinPageStaff";
+import NavLandingStaff from "./Components/NavLandingStaff";
+import NavLandingGrass from "./Components/NavLandingGrass";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Animal/>}></Route>
+        <Route path="/animal" element={<Animal />}></Route>
+        <Route path="/landingStaff" element={ <> <NavLandingStaff/> <LandingPageStaff /></> }></Route>
+        <Route
+          path="/landingGrassroot" element={ <> <NavLandingGrass/> <LandingPageGrassroot /> </>}
+        ></Route>
+       
       </Routes>
     </Router>
   );
