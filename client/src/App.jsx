a68a1c24f9c9f39dab1d6ec8f0fc3a12625e5eac
IT21016684 <110347124+KanishkaGJ@users.noreply.github.com>
2023-05-05 03:23:14 +0530

    adding componets of animal production and agriculure and creating folder structure for components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 // import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Animal from './Components/Animal';
+import Animal from './Components/AnimalProduction/Animal';
+import Beef from './Components/AnimalProduction/Beef';
+import Test from './Components/AnimalProduction/Test';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Animal/>}></Route>
+        <Route path="/beef" element={<Beef/>}></Route>
+        <Route path="/test" element={<Test/>}></Route>
       </Routes>
     </Router>
   );
---
+++ client/src/Components/Agriculture/Land.jsx
@@ -0,0 +1,95 @@
+// this is just a sample code to fill up the component
+
+
+
+
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  input: {
+    margin: theme.spacing(1),
+    width: "300px",
+  },
+}));
+
+const AutocompleteExample = () => {
+  const classes = useStyles();
+
+  const [region, setRegion] = useState("");
+  const [division, setDivision] = useState("");
+
+  const regions = [
+    { label: "Rathnapura", value: "Rathnapura" },
+    { label: "Kegalle", value: "Kegalle" },
+  ];
+
+  const divisionsByRegion = {
+    Rathnapura: [
+      { label: "Ratnapura Division 1", value: "Ratnapura Division 1" },
+      { label: "Ratnapura Division 2", value: "Ratnapura Division 2" },
+    ],
+    Kegalle: [
+      { label: "Kegalle Division 1", value: "Kegalle Division 1" },
+      { label: "Kegalle Division 2", value: "Kegalle Division 2" },
+    ],
+  };
+
+  const handleRegionChange = (event, newValue) => {
+    setRegion(newValue);
+    setDivision("");
+  };
+
+  const divisions = region
+    ? divisionsByRegion[region]
+    : [];
+
+  return (
+    <div className={classes.container}>
+      <Autocomplete
+        disablePortal
+        id="region-autocomplete"
+        options={regions}
+        getOptionLabel={(option) => option.label}
+        value={region}
+        onChange={handleRegionChange}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            className={classes.input}
+            label="Region"
+            variant="outlined"
+          />
+        )}
+      />
+      <Autocomplete
+        disablePortal
+        id="division-autocomplete"
+        options={divisions}
+        getOptionLabel={(option) => option.label}
+        value={division}
+        onChange={(event, newValue) => {
+          setDivision(newValue);
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            className={classes.input}
+            label="Division"
+            variant="outlined"
+          />
+        )}
+        disabled={!region}
+      />
+    </div>
+  );
+};
+
+export default AutocompleteExample;
